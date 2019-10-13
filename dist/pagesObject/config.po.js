e8f7789529765f4b0742efdf1b319357f68aacb1
bezrukov <bezrukov-yra@mail.ru>
2019-10-13 22:37:18 +0300

    gitIgnore

--- dist/pagesObject/config.po.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.App = {
-    user: {
-        login: 'w@w.w',
-        password: 'w'
-    },
-    url: 'http://dev2.lab-27.ru'
-};

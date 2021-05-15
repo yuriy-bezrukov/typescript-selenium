4ceb0ef9dad663632c948e9bea92ad160f55df51
brzrukov <bezrukov-yra@mail.ru>
2021-05-15 13:51:57 +0300

    add example test

--- README.md
+++ README.md
@@ -1,27 +1,13 @@
 # Automation Testing with Selenium and Typescript
 
 - install nodeJs
-- install "vs code"
-- in "vs code" File -> Open file -> "typescript-selenium"
-- edit "\typescript-selenium\tests\first.test.ts" or add test file "\typescript-selenium\tests\*.test.ts"
-
 
 1 install packages
 ```sh
 npm i
 ```
 
-2 compile typescript
-```sh
-npm run typescript
-```
-
-3 run tests
-```sh
-npm run mocha
-```
-
-or pass 2 and 3 step
+2 run tests
 ```sh
 npm run test
 ```
--- tests/login/login.test.ts
+++ tests/login/login.test.ts
@@ -18,14 +18,51 @@ capabilities.set('goog:chromeOptions', {
   ]
 });
 
-xdescribe('BAD - Authorization', function () {
+describe('BAD - Authorization', function () {
   let driver: WebDriver;
 
   before(async function () {
     driver = await new Builder().withCapabilities(capabilities).build();
-  });
+   });
 
-  xit('Login with incorrect data', async function () {
+  it('test as example autotest', async function () {
+    await driver.get('http://lab2.webtm.ru');
+    await driver.sleep(1000);
+    driver.findElement(By.css('[ng-model="ctrl.email"]')).sendKeys('w@w.w');
+    driver.findElement(By.css('[ng-model="ctrl.password"]')).sendKeys('w');
+    (await driver.findElement(By.css('[type="button"]'))).click();
+    // Ввели Логин и пароль, зашли в систему
+    await driver.sleep(2000);
+    (await driver.findElement(By.css('md-pagination-wrapper md-tab-item:nth-child(2)'))).click();
+    await driver.sleep(300);
+    (await  driver.findElement(By.css('[ng-model="ctrl.unit.hour"] option[value="14"]'))).click();
+    (await  driver.findElement(By.css('[ng-model="ctrl.unit.minute"] option[value="30"]'))).click();
+    // Ввели дату и время
+    driver.findElement(By.css('[ng-model="ctrl.unit.fam"]')).sendKeys('Рыжий'); // Фамилия 
+    driver.findElement(By.css('[ng-model="ctrl.unit.name"]')).sendKeys('Кот'); // Имя
+    driver.findElement(By.css('[placeholder="31"]')).sendKeys('3'); // день
+    driver.findElement(By.css('[placeholder="01"]')).sendKeys('4'); // месяц
+    driver.findElement(By.css('[placeholder="1901"]')).sendKeys('1994'); // год
+    await driver.sleep(1000);
+    (await driver.findElement(By.css('[ng-click="ctrl.save()"]'))).click(); //Сохранили
+    (await driver.findElement(By.css('[ng-click="exit()"]'))).click(); // Выход из учётки
+    await driver.sleep(3000);
+    
+    driver.findElement(By.css('[ng-model="ctrl.email"]')).sendKeys('w@w.w');
+    driver.findElement(By.css(('[ng-model="ctrl.password"]'))).sendKeys('w');
+    (await driver.findElement(By.css('[type="button"]'))).click();
+    await driver.sleep(3000);
+
+    // Снова вошли  систему 
+    // Видим свою запись
+
+
+    let authorizationPasswordInput = driver.findElement(By.xpath(`//div[contains(@class, 'pointer')][./div[contains(text(), '14:30')]][./div[contains(text(), 'Кот Кот / 1994.4.3 /')]]`));
+    let exitAuthorizationPasswordInput = authorizationPasswordInput.then(() => true, () => false);
+    await assert.equal(await exitAuthorizationPasswordInput, true);
+   });
+
+   xit('Login with incorrect data', async function () {
     await driver.get('http://lab2.webtm.ru');
     driver.findElement(By.css('[ng-model="ctrl.email"]')).sendKeys('w@w.w');
     driver.findElement(By.css('[ng-model="ctrl.password"]')).sendKeys('1111111111');
@@ -46,6 +83,7 @@ xdescribe('BAD - Authorization', function () {
     let exitAuthorizationPasswordInput = authorizationPasswordInput.then(() => true, () => false);
     await assert.equal(await exitAuthorizationPasswordInput, true);
   });
+
 
   after(() => driver && driver.quit());
 });
